7b1b8f99be84c078a966fc6caffe8e48218bfeac
Anton <kondalov92@gmail.com>
2023-03-28 08:06:48 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выбрать цвет\r\n";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(42, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Толщина пера";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(26, 175);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(87, 22);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Очистить область";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(170, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(613, 407);
+            this.panel1.TabIndex = 5;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MyPaint";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+
+        Color color = Color.Black; //Создаем переменную типа колор, присваиваем ей черный цвет
+        bool isPressed = false; // Логическая переменная для определения, когда можно рисовать
+        Point CurrentPoint; // Текущая точка рисунка
+        Point PrevPoint; // Начальная точка рисунка
+        Graphics g; // Создаем графический элемент
+        ColorDialog colorDialog = new ColorDialog(); // Диалоговое окно, для выбора цвета
+        public Form1()
+        {
+            InitializeComponent();
+            label2.BackColor = Color.Black; // По умолчанию, для пера, задан черный цвет, такой же цвет для фона
+            g = panel1.CreateGraphics(); //Создаем область для работы с графикой
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK) // Если окно закрылось с ОК, то меняем цвет пера и фона
+            {
+                color = colorDialog.Color;//Меняем цвет для пера
+                label2.BackColor= colorDialog.Color; // Меняем цвет для фона
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e) //Нажатие левой кнопки мыши
+        {
+            isPressed= true; //Разрешаем рисовать
+            CurrentPoint = e.Location; //Начальная точка равна текущему положению на форме
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e) //Отпускаем левую кнопку мыши
+        {
+            isPressed= false; //Запрещаем рисовать
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)//Водим курсором, с зажатой левой кнопкой мыши
+        {
+            if (isPressed)
+            {
+                PrevPoint= CurrentPoint; //Наносим первую точку на панель
+                CurrentPoint= e.Location;//Наносим вторую точку
+                my_Pen(); //Соединяем две точки линией
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh(); //Очищает панель
+        }
+
+        private void my_Pen()
+        {
+            Pen pen = new Pen(color,(float)numericUpDown1.Value); //Создаем перо, задаем ему цвет и толщину
+            g.DrawLine(pen, CurrentPoint, PrevPoint); //Соединяем точки линиями
+        }
+    }
+}
